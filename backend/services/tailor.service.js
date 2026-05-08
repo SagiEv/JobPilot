415db5e85f4134c443ac8664f6c6bca042ff4505
SagiEv <sagievroni@gmail.com>
2026-05-08 12:23:12 +0300

    localhost config

--- backend/services/tailor.service.js
+++ backend/services/tailor.service.js
@@ -6,7 +6,7 @@ const pdfParse = require('pdf-parse');
 
 const axios = require('axios');
 
-const AI_SERVICE_URL = 'http://127.0.0.1:8001';
+const AI_SERVICE_URL = process.env.AI_SERVICE_URL || 'http://localhost:8001';
 
 const runTailoring = async (userId, jobDescription, mode = 'full', useProfile = true, cvFile = null) => {
 
